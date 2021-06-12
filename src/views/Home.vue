3e7d890ed2e6638926042e199532667521481373
Lucas Mori <lucastecnew@gmail.com>
2021-06-12 00:54:46 -0300

    feat: add CountrySelect Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,16 @@
     <DataTitle :text="title" :dataDate="dataDate" />
 
     <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -14,12 +24,14 @@
 <script>
 import DataTitle from "@/components/DataTitle";
 import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -37,6 +49,17 @@ export default {
 
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
